a980a2e74bc0f3297014576d81620b6e7ed46825
Enrique Solis <enrique_solis21@hotmail.com>
2021-04-30 00:27:22 -0600

    Agregado la funcionalidad de textos dinamicos desde values/string en PerfilViewModel.kt

--- app/src/main/java/com/enrique/prueba/ui/perfil/PerfilFragment.kt
+++ app/src/main/java/com/enrique/prueba/ui/perfil/PerfilFragment.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.enrique.prueba.R
-import com.enrique.prueba.ui.favoritos.FavoritosViewModel
 
 class PerfilFragment : Fragment() {
 
@@ -23,10 +22,15 @@ class PerfilFragment : Fragment() {
         perfilViewModel =
             ViewModelProvider(this).get(PerfilViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_perfil, container, false)
-        val textView: TextView = root.findViewById(R.id.text_perfil)
-        perfilViewModel.text.observe(viewLifecycleOwner,  {
-            textView.text = it
+        val textViewTitulo: TextView = root.findViewById(R.id.textView_pefil_titulo)
+        val editTextEmail: EditText =root.findViewById(R.id.editText_perfil_email)
+        perfilViewModel.textTit.observe(viewLifecycleOwner,  {
+            textViewTitulo.text = it
+          })
+        perfilViewModel.hintEm.observe(viewLifecycleOwner,{
+            editTextEmail.hint=it
         })
+
         return root
     }
 }
--- app/src/main/java/com/enrique/prueba/ui/perfil/PerfilViewModel.kt
+++ app/src/main/java/com/enrique/prueba/ui/perfil/PerfilViewModel.kt
@@ -1,13 +1,22 @@
 package com.enrique.prueba.ui.perfil
 
+import android.app.Application
+import androidx.core.content.res.TypedArrayUtils.getString
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.enrique.prueba.R
 
-class PerfilViewModel: ViewModel() {
+class PerfilViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Perfil Fragment"
+    private val _textTit = MutableLiveData<String>().apply {
+        this.value=getApplication<Application>().getString(R.string.text_perfil_titulo)
     }
-    val text: LiveData<String> = _text
+    private val _hintEmail = MutableLiveData<String>().apply {
+        this.value=getApplication<Application>().getString(R.string.email_hint)
+    }
+    var textTit: LiveData<String> = _textTit
+    var hintEm: LiveData<String> = _hintEmail
+
 }
