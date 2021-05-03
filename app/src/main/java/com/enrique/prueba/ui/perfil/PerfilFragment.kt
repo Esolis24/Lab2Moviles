acb71079725be70f68e5f8bc2bb197ff32de68d1
Enrique Solis <enrique_solis21@hotmail.com>
2021-05-03 11:42:23 -0600

    Agregados los componentes visuales de Perfil

--- app/src/main/java/com/enrique/prueba/ui/perfil/PerfilFragment.kt
+++ app/src/main/java/com/enrique/prueba/ui/perfil/PerfilFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.enrique.prueba.R
+import org.w3c.dom.Text
 
 class PerfilFragment : Fragment() {
 
@@ -24,13 +26,33 @@ class PerfilFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_perfil, container, false)
         val textViewTitulo: TextView = root.findViewById(R.id.textView_pefil_titulo)
         val editTextEmail: EditText =root.findViewById(R.id.editText_perfil_email)
+        val editTextPassword: EditText=root.findViewById(R.id.editText_perfil_password)
+        val textViewForgetPassword: TextView=root.
+        findViewById(R.id.textView_perfil_password_olvidada)
+        val buttonLogin: Button =root.findViewById(R.id.button_perfil_login)
+        val textViewNewUser: TextView=root.findViewById(R.id.textView_perfil_no_tiene_cuenta)
+        val buttonSignUp: Button=root.findViewById(R.id.button_perfil_registro)
         perfilViewModel.textTit.observe(viewLifecycleOwner,  {
             textViewTitulo.text = it
           })
         perfilViewModel.hintEm.observe(viewLifecycleOwner,{
             editTextEmail.hint=it
         })
-
+        perfilViewModel.hintPass.observe(viewLifecycleOwner,{
+            editTextPassword.hint=it
+        })
+        perfilViewModel.textForgetPass.observe(viewLifecycleOwner,{
+            textViewForgetPassword.text=it
+        })
+        perfilViewModel.textLogin.observe(viewLifecycleOwner,{
+            buttonLogin.text=it
+        })
+        perfilViewModel.textNewUser.observe(viewLifecycleOwner,{
+            textViewNewUser.text=it
+        })
+        perfilViewModel.textSignUp.observe(viewLifecycleOwner,{
+            buttonSignUp.text=it
+        })
         return root
     }
 }
--- app/src/main/java/com/enrique/prueba/ui/perfil/PerfilViewModel.kt
+++ app/src/main/java/com/enrique/prueba/ui/perfil/PerfilViewModel.kt
@@ -1,11 +1,9 @@
 package com.enrique.prueba.ui.perfil
 
 import android.app.Application
-import androidx.core.content.res.TypedArrayUtils.getString
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.enrique.prueba.R
 
 class PerfilViewModel(application: Application) : AndroidViewModel(application) {
@@ -14,9 +12,28 @@ class PerfilViewModel(application: Application) : AndroidViewModel(application) 
         this.value=getApplication<Application>().getString(R.string.text_perfil_titulo)
     }
     private val _hintEmail = MutableLiveData<String>().apply {
-        this.value=getApplication<Application>().getString(R.string.email_hint)
+        this.value=getApplication<Application>().getString(R.string.perfil_email_hint)
+    }
+    private val _hintPassword = MutableLiveData<String>().apply {
+        this.value=getApplication<Application>().getString(R.string.perfil_password_hint)
+    }
+    private val _textForgetPassword=MutableLiveData<String>().apply {
+        this.value=getApplication<Application>().getString(R.string.perfil_password_olvidada)
+    }
+    private val _textLoginButton=MutableLiveData<String>().apply {
+        this.value=getApplication<Application>().getString(R.string.perfil_login_button)
+    }
+    private val _textNewUser=MutableLiveData<String>().apply {
+        this.value = getApplication<Application>().getString(R.string.perfil_sin_cuenta)
+    }
+    private val _textSignUpButton=MutableLiveData<String>().apply {
+        this.value = getApplication<Application>().getString(R.string.perfil_registro_button)
     }
     var textTit: LiveData<String> = _textTit
     var hintEm: LiveData<String> = _hintEmail
-
+    var hintPass: LiveData<String> = _hintPassword
+    var textForgetPass: LiveData<String> =_textForgetPassword
+    var textLogin: LiveData<String> = _textLoginButton
+    var textNewUser: LiveData<String> =_textNewUser
+    var textSignUp: LiveData<String> =_textSignUpButton
 }
