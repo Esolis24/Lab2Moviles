8e77df838d8f910def26b1ce736de511d9d2c16e
Enrique Solis <enrique_solis21@hotmail.com>
2021-05-05 22:01:24 -0600

    NO TOCAR

--- app/src/main/java/com/enrique/prueba/ui/explorar/ExplorarFragment.kt
+++ app/src/main/java/com/enrique/prueba/ui/explorar/ExplorarFragment.kt
@@ -1,19 +1,25 @@
 package com.enrique.prueba.ui.explorar
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.enrique.prueba.CustomAdapter
 import com.enrique.prueba.R
+import kotlinx.android.synthetic.main.fragment_explorar.*
 
 class ExplorarFragment : Fragment() {
 
     private lateinit var explorarViewModel: ExplorarViewModel
-
+    private var customAdapter: CustomAdapter?=null
+    companion object{
+        var myList: ArrayList<Tour> = arrayListOf()
+    }
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -22,10 +28,34 @@ class ExplorarFragment : Fragment() {
         explorarViewModel =
                 ViewModelProvider(this).get(ExplorarViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_explorar, container, false)
-        val textView: TextView = root.findViewById(R.id.text_explorar)
+
         explorarViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
+            explorar_search.queryHint=it
         })
         return root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d("testeo","Entramos")
+        super.onViewCreated(view, savedInstanceState)
+        customAdapter= CustomAdapter(myList)
+        var layoutManager: RecyclerView.LayoutManager?=
+                LinearLayoutManager(this.context,
+                        RecyclerView.VERTICAL, false)
+              my_recycler_view.layoutManager=layoutManager
+               my_recycler_view.adapter=customAdapter
+            if(customAdapter!!.itemCount<=0) {
+                var aux: ArrayList<Tour> =arrayListOf()
+                val tour1 = Tour("Aurora Boreal, Finlandia", 1250.50f, "tour1.jpg")
+                val tour2 = Tour("Isla del Coco, Costa Rica", 2999.99f, "tour2.jpg")
+                val tour3 = Tour("Berna, Suiza", 1600.33f, "tour3.jpg")
+                aux.add(tour1)
+                aux.add(tour2)
+                aux.add(tour3)
+                myList= aux.toMutableList() as ArrayList<Tour>
+                customAdapter?.updateTours(myList)
+            }
+
     }
 }
--- app/src/main/java/com/enrique/prueba/ui/explorar/ExplorarViewModel.kt
+++ app/src/main/java/com/enrique/prueba/ui/explorar/ExplorarViewModel.kt
@@ -1,13 +1,20 @@
 package com.enrique.prueba.ui.explorar
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.enrique.prueba.CustomAdapter
+import com.enrique.prueba.R
+import kotlinx.android.synthetic.main.fragment_explorar.*
 
-class ExplorarViewModel : ViewModel() {
+class ExplorarViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is explorar Fragment"
+        this.value=getApplication<Application>().getString(R.string.query_hint)
     }
     val text: LiveData<String> = _text
 }
