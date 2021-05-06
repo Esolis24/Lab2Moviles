5b1bdf427ccd444b196f79552d8455bb8ccd8555
Enrique Solis <enrique_solis21@hotmail.com>
2021-05-05 23:29:26 -0600

    NO TOCAR PARTE 2

--- app/src/main/java/com/enrique/prueba/ui/explorar/ExplorarFragment.kt
+++ app/src/main/java/com/enrique/prueba/ui/explorar/ExplorarFragment.kt
@@ -1,20 +1,29 @@
 package com.enrique.prueba.ui.explorar
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.enrique.prueba.CustomAdapter
 import com.enrique.prueba.R
+import com.enrique.prueba.ui.dialog.DatePickerFragment
 import kotlinx.android.synthetic.main.fragment_explorar.*
+import java.text.DateFormatSymbols
+import java.util.*
 
-class ExplorarFragment : Fragment() {
-
+class ExplorarFragment : DialogFragment() {
+    private val c: Calendar= Calendar.getInstance()
+    private var dpd: DatePickerDialog?=null
     private lateinit var explorarViewModel: ExplorarViewModel
     private var customAdapter: CustomAdapter?=null
     companion object{
@@ -30,14 +39,20 @@ class ExplorarFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_explorar, container, false)
 
         explorarViewModel.text.observe(viewLifecycleOwner, {
-            explorar_search.queryHint=it
+            explorar_search.queryHint = it
+        })
+        explorarViewModel.ida.observe(viewLifecycleOwner, {
+            fecha_salida.hint = it
+        })
+        explorarViewModel.vuelta.observe(viewLifecycleOwner, {
+            fecha_regreso.hint = it
         })
         return root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("testeo","Entramos")
+        Log.d("testeo", "Entramos")
         super.onViewCreated(view, savedInstanceState)
         customAdapter= CustomAdapter(myList)
         var layoutManager: RecyclerView.LayoutManager?=
@@ -56,6 +71,21 @@ class ExplorarFragment : Fragment() {
                 myList= aux.toMutableList() as ArrayList<Tour>
                 customAdapter?.updateTours(myList)
             }
+            fecha_salida.setOnClickListener{
+                showDatePickerDialog(fecha_salida)
+            }
+            fecha_regreso.setOnClickListener {
+                showDatePickerDialog(fecha_regreso)
+            }
 
+    }
+    private fun showDatePickerDialog(fecha: TextView) {
+        val newFragment = DatePickerFragment.newInstance(DatePickerDialog.OnDateSetListener { _, year, month, day ->
+            // +1 because January is zero
+            val selectedDate = "${day.toString()}-${DateFormatSymbols().months[month-1].substring(0,3)}-${year}"
+            fecha.text = selectedDate
+        })
+
+        newFragment.show(requireActivity().supportFragmentManager, "datePicker")
     }
 }
--- app/src/main/java/com/enrique/prueba/ui/explorar/ExplorarViewModel.kt
+++ app/src/main/java/com/enrique/prueba/ui/explorar/ExplorarViewModel.kt
@@ -16,5 +16,14 @@ class ExplorarViewModel(application: Application) : AndroidViewModel(application
     private val _text = MutableLiveData<String>().apply {
         this.value=getApplication<Application>().getString(R.string.query_hint)
     }
+    private val _ida_hint = MutableLiveData<String>().apply{
+        this.value=getApplication<Application>().getString(R.string.date_init_hint)
+    }
+    private val _vuelta_hint = MutableLiveData<String>().apply{
+        this.value=getApplication<Application>().getString(R.string.date_return_hint)
+    }
     val text: LiveData<String> = _text
+    val ida: LiveData<String> = _ida_hint
+    val vuelta: LiveData<String> = _vuelta_hint
+
 }
